809f7bec340f6b581249127c2abdf66cecbda7c2
rusty0515 <c20222799.decolongon@csav.edu.ph>
2025-03-30 11:09:28 +0800

    update edit resource

--- app/Filament/Resources/VetAppointment/AppointmentApplicationResource/Pages/EditAppointmentApplication.php
+++ app/Filament/Resources/VetAppointment/AppointmentApplicationResource/Pages/EditAppointmentApplication.php
@@ -16,4 +16,9 @@ class EditAppointmentApplication extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
